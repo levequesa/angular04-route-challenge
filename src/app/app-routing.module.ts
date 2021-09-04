8fa6ae483a3addd88ee9b25c8dbffd7d3b4c57e4
levequesa <79331585+levequesa@users.noreply.github.com>
2021-09-04 19:46:47 +0200

    ng - route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SignupComponent } from './signup/signup.component';
+import { UserComponent } from './user/user.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: MenuComponent},
+  {path:'signup', component:SignupComponent},
+  {path:'user', component:UserComponent},
+  {path:'**', redirectTo:'/user', pathMatch: 'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserComponent } from './user/user.component';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    SignupComponent,
+    UserComponent
   ],
   imports: [
     BrowserModule,
